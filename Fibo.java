62c522df58e67f4d4ec23369c53e1601ecb991ab
vidhya <vidhyamani24032007@gmail.com>
2026-07-29 22:19:47 +0530

    changes made

---
+++ Fibo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args){
+        try(Scanner in = new Scanner(System.in)){
+            int n = in.nextInt(); 
+            int p = 0;
+            int i = 1;
+            int count = 2;
+
+            while(count <= n){
+                int temp=i;
+                i=p+i;
+                p=temp;
+                count++;
+            }
+
+            System.out.println(i);
+
+            in.close();
+            }
+        }
+    }
+
