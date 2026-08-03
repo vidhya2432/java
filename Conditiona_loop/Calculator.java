27856b5fcc126b71bbaa7595264eed47c1cef7d2
vidhya <vidhyamani24032007@gmail.com>
2026-08-03 06:50:22 +0530

    chnages made

--- Conditiona_loop/Calculator.java
+++ Conditiona_loop/Calculator.java
@@ -2,51 +2,57 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-    
-        try(Scanner in = new Scanner(System.in)) {
-            int ans = 0;
-            
-        while(true) {
-                // take the operator as input
-            System.out.print("Enter the operator: ");
-            char op = in.next().trim().charAt(0);
-            if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
-                // take the numbers as input
-                System.out.print("Enter two numbers: ");
-                int num1 = in.nextInt();
-                int num2 = in.nextInt();
 
-                if(op == '+'){
-                    ans = num1 + num2;
-                    System.out.println(ans);
-                }
-                else if(op == '-') {
-                    ans = num1 - num2;
-                    System.out.println(ans);
-                }
-                else if(op == '*') {
-                    ans = num1 * num2;
-                    System.out.println(ans);
-                }
-                else if(op == '/') {
-                    ans = num1 / num2;
-                    System.out.println(ans);
-                }
-                else
-                if(op == '%') {
-                    ans = num1 % num2;
-                    System.out.println(ans);
-                }
-                else if(op =='x'|| op == 'X') {
+        try (Scanner in = new Scanner(System.in)) {
+
+            while (true) {
+
+                System.out.print("Enter the operator (+, -, *, /, %, X to Exit): ");
+                char op = in.next().trim().charAt(0);
+
+                // Exit condition
+                if (op == 'X' || op == 'x') {
+                    System.out.println("Calculator Closed.");
                     break;
                 }
-                else {
-                        System.out.println("Invalid operation.");
+
+                // Check for valid operator
+                if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+
+                    System.out.print("Enter two numbers: ");
+                    int num1 = in.nextInt();
+                    int num2 = in.nextInt();
+
+                    int ans = 0;
+
+                    if (op == '+') {
+                        ans = num1 + num2;
+                    } else if (op == '-') {
+                        ans = num1 - num2;
+                    } else if (op == '*') {
+                        ans = num1 * num2;
+                    } else if (op == '/') {
+                        if (num2 != 0) {
+                            ans = num1 / num2;
+                        } else {
+                            System.out.println("Cannot divide by zero.");
+                            continue;
+                        }
+                    } else if (op == '%') {
+                        if (num2 != 0) {
+                            ans = num1 % num2;
+                        } else {
+                            System.out.println("Cannot find remainder with zero.");
+                            continue;
+                        }
                     }
+
+                    System.out.println("Result = " + ans);
+
+                } else {
+                    System.out.println("Invalid operator.");
                 }
             }
-            System.out.println(ans);
-            in.close();
         }
     }
 }
