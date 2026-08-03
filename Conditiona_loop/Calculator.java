9c481eca29bd196eca6abb98aa378971e102d5b5
vidhya <vidhyamani24032007@gmail.com>
2026-08-03 06:15:03 +0530

    changes made

--- Conditiona_loop/Calculator.java
+++ Conditiona_loop/Calculator.java
@@ -2,54 +2,51 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+    
         try(Scanner in = new Scanner(System.in)) {
             int ans = 0;
             
-            while(true) {
+        while(true) {
                 // take the operator as input
-                System.out.print("Enter the operator: ");
-                char op = in.next().trim().charAt(0);
-                if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
+            if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
                 // take the numbers as input
                 System.out.print("Enter two numbers: ");
                 int num1 = in.nextInt();
                 int num2 = in.nextInt();
 
-                if(op == '+') {
+                if(op == '+'){
                     ans = num1 + num2;
+                    System.out.println(ans);
                 }
                 else if(op == '-') {
                     ans = num1 - num2;
+                    System.out.println(ans);
                 }
                 else if(op == '*') {
                     ans = num1 * num2;
+                    System.out.println(ans);
                 }
                 else if(op == '/') {
                     ans = num1 / num2;
+                    System.out.println(ans);
                 }
                 else
                 if(op == '%') {
-                    ans = num1 % num2;   
+                    ans = num1 % num2;
+                    System.out.println(ans);
                 }
                 else if(op =='x'|| op == 'X') {
                     break;
                 }
                 else {
                         System.out.println("Invalid operation.");
-                        continue;
                     }
                 }
-                else {
-                    System.out.println("Invalid operator");
-                    continue;
-                }
-
-                System.out.println(ans);
-
-                in.close();
             }
-   
+            System.out.println(ans);
+            in.close();
         }
     }
 }
-
