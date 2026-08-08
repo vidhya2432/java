a0287941037052e7b1b5917b486f96a5b9747b64
vidhya <vidhyamani24032007@gmail.com>
2026-08-08 07:32:13 +0530

    changes made in swap

--- Functions/Swap.java
+++ Functions/Swap.java
@@ -1,23 +1,24 @@
-public class Swap {
-    public static void main(String[] args){
-        int a = 20;
-        int b = 30;
-
         //swap number code
 //        int temp = a;
 //        a = b;
 //        b = temp;
+public class Swap {
 
+    public static void main(String[] args) {
 
-        swap(a,b);
-        System.out.println(a+" "+b);
+        int a = 20;
+        int b = 30;
+
+        swap(a, b);
 
     }
 
-    static void swap(int a , int b){
+    static void swap(int a, int b) {
+
         int temp = a;
         a = b;
         b = temp;
 
+        System.out.println(a + " " + b);
     }
 }
